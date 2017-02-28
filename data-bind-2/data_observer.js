7fddce2718c06c8ed2432404b844f480ac57514a
Kelly <13531957084@163.com>
2017-02-28 19:46:23 +0800

    完成动态数据绑定任务三

--- data-bind-2/data_observer.js
+++ data-bind-2/data_observer.js
@@ -25,13 +25,29 @@ class EventEmiter{
         }
 }
 
+// 动态数据绑定
 class Observer{
         constructor( data ){
                 this.data = data
+                this.key_value = null
                 this.event_emit = new EventEmiter(  )
+                // 监听 data 对象每一个属性的值
                 this.traverse( data )
         }
-        // 属性遍历
+        // 获取对象指定属性值
+        receive_value( obj, name ){
+                for( let key in obj ){
+                        if( key === name ){
+                                this.key_value = obj[ key ]
+                                break
+                        } else {
+                                if( typeof( obj[ key ] ) === "object" ){
+                                        this.receive_value( obj[ key ], name )
+                                }
+                        }
+                }
+        }
+        // 遍历属性添加 监听/事件绑定
         traverse( obj ){
                 let value
                 for( let key in obj ){
@@ -46,21 +62,23 @@ class Observer{
         }
         // 数据观测
         data_observer( obj, key, value ){
+                let val = value
                 let _this = this
                 Object.defineProperty(obj, key, {
                         enumerable : true,
                         configurable : true,
                         get : function(  ){
                                 console.log( "你访问了" + key )
-                                return value
+                                return val
                         },
                         set : function( new_value ){
                                 console.log( "你设置了" + key + ",新值为：" + new_value )
-                                if( value === new_value ){ return }
+                                if( val === new_value ){ return }
                                 if( typeof( new_value ) === "object" ){
                                         _this.traverse( new_value )
                                 }
-                                value = new_value
+                                val = new_value
+                                // 触发监听事件
                                 for( let event in _this.event_emit.events ){
                                         if( event === key ){
                                                 _this.event_emit.emit( key, new_value )
@@ -69,8 +87,17 @@ class Observer{
                         }
                 })
         }
-        // 数据监听
+        // 事件绑定
         $watch( property, callback ){
+                // 获取 property 的值，并存放在 this.key_value 中
+                this.receive_value( this.data, property )
+                let key_value = this.key_value
+                if( typeof( key_value ) === "object" ){
+                        // 为所有子属性绑定相同事件
+                        for( let k in key_value ){
+                                this.event_emit.on( k, callback )
+                        }
+                }
                 this.event_emit.on( property, callback )
         }
 }
@@ -86,7 +113,7 @@ app.data.user.name = {
         first_name : "zepeng",
 }
 app.data.user.name.first_name
-app.$watch("first_name", function( first_name ){
-        console.log( "我的名字变啦，现在是" + first_name )
+app.$watch("name", function( new_name ){
+        console.log( "我的姓名发生变化啦，可能是姓氏发生变化，也可能是名字发生变化" )
 })
 app.data.user.name.first_name = "L"
